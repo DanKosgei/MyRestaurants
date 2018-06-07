8170b351ccb3d1abca53dab04328d54fab3f62af
dankosgei <dkosgei6@gmail.com>
2018-06-07 12:25:05 +0300

    refactored MainActvity and Restaurant Activity.java

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -1,13 +1,19 @@
 package com.epicodus.myrestaurants;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+    @Bind(R.id.listView) ListView mListView;
+
+    private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
+            "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
+            "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-    }
-}
+        ButterKnife.bind(this);
