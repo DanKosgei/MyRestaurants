d36c4d56e4afd80b520551851b0267a71c12bd9c
dankosgei <dkosgei6@gmail.com>
2018-06-12 15:39:31 +0300

    added Recycleviews

---
+++ app/src/main/java/com/epicodus/myrestaurants/Adapters/RestaurantListAdapter.java
@@ -0,0 +1,11 @@
+package com.epicodus.myrestaurants.Adapters;
+
+public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+    private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+    private Context mContext;
+
+    public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
+        mContext = context;
+        mRestaurants = restaurants;
+    }
+}
